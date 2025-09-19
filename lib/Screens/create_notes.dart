a62650d27e20a5496a5d28d292f833420d376f54
Sagar Dhadke <peaceunique2003@gmail.com>
2025-09-19 23:21:05 +0530

    Fixed Scroll Elevation

--- lib/Screens/create_notes.dart
+++ lib/Screens/create_notes.dart
@@ -13,6 +13,7 @@ class CreateNotes extends StatelessWidget {
       backgroundColor: Color(0XFF252525),
       appBar: AppBar(
         backgroundColor: Color(0XFF252525),
+        scrolledUnderElevation: 0,
         leading: Padding(
           padding: const EdgeInsets.only(left: 15.0),
           child: InkWell(
