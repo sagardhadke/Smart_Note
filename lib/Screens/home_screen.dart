349b37fdb5f243f75585570ec41fc8529420c67d
Sagar Dhadke <peaceunique2003@gmail.com>
2025-09-27 19:29:11 +0530

    Added Fetch Smart note and delete note Feature in app

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:smart_note/Routes/app_routes.dart';
-import 'package:smart_note/data/constants.dart';
+import 'package:smart_note/database/db_helper.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DBHelper? dbRef;
+  List<Map<String, dynamic>> getAllNotes = [];
+  DateFormat df = DateFormat.yMMMEd();
+
+  @override
+  void initState() {
+    super.initState();
+    dbRef = DBHelper.getInstance;
+    fetchAllNote();
+  }
+
+  Future<void> fetchAllNote() async {
+    getAllNotes = await dbRef!.fetchAllSmartNotes();
+    print("All Smart Note $getAllNotes");
+    setState(() {});
+  }
+
+  void formateDate() {}
 
   @override
   Widget build(BuildContext context) {
@@ -34,55 +59,118 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       backgroundColor: Color(0XFF252525),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisSpacing: 15,
-          crossAxisSpacing: 10,
-        ),
-        itemCount: Constants.myNotes.length,
-        itemBuilder: (BuildContext context, int index) {
-          final bgColor = Constants.myNotes[index]['bgColor'];
-          return InkWell(
-            onTap: () => Navigator.pushNamed(
-              context,
-              AppRoutes.notesDetails,
-              arguments: Constants.myNotes[index],
-            ),
-            child: Container(
-              padding: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color.fromARGB(255, bgColor[0], bgColor[1], bgColor[2]),
+      body: getAllNotes.isNotEmpty
+          ? GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 15,
+                crossAxisSpacing: 10,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    Constants.myNotes[index]['title'],
-                    maxLines: 4,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
+              padding: EdgeInsets.all(7),
+              // itemCount: Constants.myNotes.length,
+              itemCount: getAllNotes.length,
+              itemBuilder: (BuildContext context, int index) {
+                // final bgColor = Constants.myNotes[index]['bgColor'];
+                return InkWell(
+                  onTap: () => Navigator.pushNamed(
+                    context,
+                    AppRoutes.notesDetails,
+                    // arguments: Constants.myNotes[index],
+                    arguments: getAllNotes[index],
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      // color: Color.fromARGB(255, bgColor[0], bgColor[1], bgColor[2]),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          // Constants.myNotes[index]['title'],
+                          getAllNotes[index][DBHelper.smartNoteTitle],
+                          maxLines: 4,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          // Constants.myNotes[index]['date'],
+                          df.format(
+                            DateTime.fromMillisecondsSinceEpoch(
+                              int.parse(
+                                getAllNotes[index][DBHelper.smartNoteCreatedAt],
+                              ),
+                            ),
+                          ),
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Spacer(),
+                        Align(
+                          alignment: AlignmentGeometry.bottomRight,
+                          child: IconButton(
+                            onPressed: () async {
+                              bool isNoteDeleted = await dbRef!.deleteSmartNote(
+                                id: getAllNotes[index][DBHelper.smartNoteId],
+                              );
+                              if (isNoteDeleted) {
+                                fetchAllNote();
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(
+                                      "Smart Note Has Been Deleted Successfully",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    backgroundColor: Colors.green,
+                                    duration: Duration(seconds: 2),
+                                  ),
+                                );
+                              }
+                            },
+                            icon: Icon(
+                              CupertinoIcons.delete,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 10),
+                );
+              },
+            )
+          : Center(
+              child: Column(
+                spacing: 15,
+                children: [
                   Text(
-                    Constants.myNotes[index]['date'],
+                    'No Notes Available',
                     style: TextStyle(
-                      color: Colors.black,
+                      color: Colors.white,
                       fontSize: 15,
                       fontWeight: FontWeight.w400,
                     ),
                   ),
+                  ElevatedButton(
+                    onPressed: () => fetchAllNote(),
+                    child: Text("Fetch All Note"),
+                  ),
                 ],
               ),
             ),
-          );
-        },
-      ),
 
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.pushNamed(context, AppRoutes.createNotes),
